cb1e603fd94a501292570d0b9bb2e5002ee3a5a7
Benjamin-Gruber <83592290+Benjamin-Gruber@users.noreply.github.com>
2022-01-30 14:23:38 +0100

    v-app-bar

--- Watching Birds Angabe/client/src/App.vue
+++ Watching Birds Angabe/client/src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <v-app>
+    <v-app-bar class="blue darken-1" app>
+      <img src="./assets/bird.png" width="50" class="mr-3" />
+      <v-toolbar-title class="white--text">BIRD WATCH</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" class="mr-3" to="/">Home</v-btn>
+      <v-btn color="primary" class="mr-3">Gallery</v-btn>
+      <v-btn color="primary" to="/about">About</v-btn>
+    </v-app-bar>
     <v-main>
       <v-container> <router-view /></v-container>
     </v-main>
--- Watching Birds Angabe/client/src/views/Home.vue
+++ Watching Birds Angabe/client/src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <p class="text-h3 text-center mt-4">
-      <v-icon class="text-h3 red--text">mdi-heart</v-icon> Willkommen!
-      <v-icon class="text-h3 red--text">mdi-heart</v-icon>
-    </p>
+
   </div>
 </template>
 
