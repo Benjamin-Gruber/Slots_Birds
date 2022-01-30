bee1b415442cf0fc50223732a97d9bf5dedd844a
Benjamin-Gruber <83592290+Benjamin-Gruber@users.noreply.github.com>
2022-01-30 14:28:54 +0100

    getBirds in App

--- Watching Birds Angabe/client/src/App.vue
+++ Watching Birds Angabe/client/src/App.vue
@@ -5,7 +5,7 @@
       <v-toolbar-title class="white--text">BIRD WATCH</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn color="primary" class="mr-3" to="/">Home</v-btn>
-      <v-btn color="primary" class="mr-3">Gallery</v-btn>
+      <v-btn color="primary" class="mr-3" to="/gallery">Gallery</v-btn>
       <v-btn color="primary" to="/about">About</v-btn>
     </v-app-bar>
     <v-main>
@@ -15,11 +15,32 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'App',
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      birds: [],
+    };
+  },
+
+  created () {
+    this.getBirds();
+  },
+
+  methods: {
+    async getBirds() {
+      try {
+        const { data } = await axios({
+          url: 'http://127.0.0.1:3000/birds',
+          method: 'GET',
+        });
+        this.birds = data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 };
 </script>
